8eb93b2b27ac31cef2929c826371e442bddc54fc
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2022-11-19 13:27:09 +0100

    up button smooth scroll working

--- src/components/parts/MainComp.js
+++ src/components/parts/MainComp.js
@@ -6,33 +6,23 @@ import SecondSection from "./SecondSection";
 import FourthSection from "./FourthSection";
 import Cookies from "./Cookies";
 
-const OtherComponent = loadable(() => import("./ThirdSection"));
+const ThirdSectionComp = loadable(() => import("./ThirdSection"));
+const BackToTopComp = loadable(() => import("./BackToTop"));
 
 export function MainComp() {
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
   return (
     <html lang="en" className="light scroll-smooth" dir="ltr">
-      <body className="font-nunito text-base text-black dark:text-white dark:bg-slate-900">
+      <body className="font-nunito scroll-smooth text-base text-black dark:text-white dark:bg-slate-900">
         {/* SECTION 1 */}
         <FirstSection />
         {/* SECTION 2 */}
         <SecondSection />
         {/* SECTION 3 */}
-        <OtherComponent />
+        <ThirdSectionComp />
         {/* SECTION 4 */}
         <FourthSection />
         {/* BACK TO TOP */}
-        <a
-          href="#"
-          onclick={topFunction}
-          id="back-to-top"
-          className="back-to-top fixed hidden text-lg rounded-full z-10 bottom-5 right-5 h-9 w-9 text-center bg-indigo-600 text-white leading-9"
-        >
-          <i className="uil uil-arrow-up"></i>
-        </a>
+        <BackToTopComp />
         {/* COOKIES  */}
         <Cookies />
       </body>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { MainComp } from "../components/parts/MainComp";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title="Corso | Documentation"
       description="Documentation for Corso, a free, secure, and open-source backup tool for Microsoft 365"
     >
-      <main>
-        <MainComp />
-      </main>
+      <MainComp />
     </Layout>
   );
 }
