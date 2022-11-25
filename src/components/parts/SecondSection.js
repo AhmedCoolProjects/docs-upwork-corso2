fbf815d99a5b4fd54c8ae27b5ac164238a55c298
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2022-11-25 08:21:54 +0100

    second section theming

--- src/components/parts/SecondSection.js
+++ src/components/parts/SecondSection.js
@@ -6,7 +6,7 @@ export default function SecondSection() {
     <section className="relative flex !tracking-wide  flex-col items-center overflow-hidden">
       <div className="!container relative">
         <div className="flex flex-col content-center items-center justify-start relative md:mt-24 mt-16 text-center">
-          <div className="wow w-[95%] sm:w-[80%] animate__animated relative  animate__fadeIn">
+          <div className="wow w-[95%] shadow-xl sm:w-[80%] animate__animated relative  animate__fadeIn">
             <div className="flex flex-row items-center bg-gray-200 rounded-t-lg h-6">
               <div className="align-middle flex flex-col items-center justify-center">
                 <img className="h-4 px-2" src="assets/images/powershell.svg" />
